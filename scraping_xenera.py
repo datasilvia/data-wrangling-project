9a481377a898d2e0458d8a4e0f843127098b738f
Jotis86 <juaneloyduranbon@hotmail.com>
2024-10-25 10:47:45 +0200

    Xenera

--- scraping_xenera.py
+++ scraping_xenera.py
@@ -27,26 +27,31 @@ driver.quit()
 soup = BeautifulSoup(html, 'html.parser')
 
 # Paso 4: Extraer la información específica
-# Ajustar los selectores CSS según la estructura HTML actual de la página
-energia_element = soup.select_one("ul.space-y-2 li")
+# Buscar los elementos de energía basados en el texto contenido
+energia_valle_element = soup.find("span", string="Valle")
+energia_llano_element = soup.find("span", string="Llano")
+energia_punta_element = soup.find("span", string="Punta")
+
+# Verificar que los elementos existen antes de acceder a su texto
+energia_valle = energia_valle_element.find_next("li").text.strip() if energia_valle_element else "No encontrado"
+energia_llano = energia_llano_element.find_next("li").text.strip() if energia_llano_element else "No encontrado"
+energia_punta = energia_punta_element.find_next("li").text.strip() if energia_punta_element else "No encontrado"
 potencia_element = soup.select_one("div.border-t-primary-light ul li")
 coste_gestion_element = soup.select_one("div.border-t-primary-light p.font-bold")
 
-# Verificar que los elementos existen antes de acceder a su texto
-energia = energia_element.text.strip() if energia_element else "No encontrado"
 potencia = potencia_element.text.strip() if potencia_element else "No encontrado"
 coste_gestion = coste_gestion_element.text.strip() if coste_gestion_element else "No encontrado"
 
 # Paso 5: Crear un DataFrame de pandas y guardar los resultados en un CSV
 data = {
-    "Energía": [energia],
+    "Energía Valle": [energia_valle],
+    "Energía Llano": [energia_llano],
+    "Energía Punta": [energia_punta],
     "Potencia": [potencia],
     "Coste de gestión": [coste_gestion]
 }
 
 df = pd.DataFrame(data)
 df.to_csv("resultados_tarifas_xenera.csv", index=False)
-
-df
 
 print("Datos guardados en resultados_tarifas_xenera.csv")
